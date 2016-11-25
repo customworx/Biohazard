5907163c1364902f1cff0f1195b18c387597749b
pierx <piereralph@gmail.com>
2016-11-25 11:34:05 +0800

    Update Keep, plugin and gradle

--- Theme/src/main/java/com/pierx/biohazard/SubstratumLauncher.java
+++ Theme/src/main/java/com/pierx/biohazard/SubstratumLauncher.java
@@ -7,93 +7,16 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.github.javiersantos.piracychecker.PiracyChecker;
-import com.github.javiersantos.piracychecker.PiracyCheckerUtils;
-import com.github.javiersantos.piracychecker.enums.InstallerID;
-import com.github.javiersantos.piracychecker.enums.PiracyCheckerCallback;
-import com.github.javiersantos.piracychecker.enums.PiracyCheckerError;
 
 /**
  * @author Nicholas Chum (nicholaschum)
  */
 
 public class SubstratumLauncher extends Activity {
-
-    // < STATIC THEMER CRUISE CONTROL >
-    // On Android Studio, please open the bottom window tab before continuing: TODO
-    // You MUST complete ALL 5 steps!
-    //
-    // TODO: Themers, this is your FIRST step
-    // UNIVERSAL SWITCH: Control whether Anti-Piracy should be activated while testing
-    private static final boolean ENABLE_ANTI_PIRACY = true;
-    // In order to retrieve your BASE64 license key your app must be uploaded to
-    // Play Developer Console. Then access to your app -> Services and APIs.
-    // You will need to replace "" with the code you obtained from the Play Developer Console.
-    // If ENABLE_ANTI_PIRACY is false, you may skip this
-    // TODO: Themers, this is your SECOND step
-    private static final String BASE_64_LICENSE_KEY = "";
-    // Build your signed APK using your signature, and run the app once in Substratum
-    // (open your theme). Check your logcat!
-    // You will need to do this only when submitting to Play. Locate "SubstratumAntiPiracyLog" and
-    // head down to line 51. You will need to replace "" with the code you obtained from your
-    // logcat.
-    // If ENABLE_ANTI_PIRACY is false, you may skip this
-    // TODO: Themers, this is your THIRD step
-    private static final String APK_SIGNATURE_PRODUCTION = "hrjeFoblEJF8ruCVKJIe1JX9ba8=";
-    //
-    // END OF STATIC THEMER CRUISE CONTROL
-
-    private void startAntiPiracyCheck() {
-        // TODO: Themers, this is your FOURTH step
-//        Log.e("SubstratumAntiPiracyLog", PiracyCheckerUtils.getAPKSignature(this));
-        // COMMENT OUT THE ABOVE LINE ONCE YOU OBTAINED YOUR APK SIGNATURE USING
-        // TWO DASHES (LIKE THIS EXACT LINE)
-
-        PiracyChecker piracyChecker = new PiracyChecker(this)
-
-                // TODO: Themers, this is your FINAL step
-                // To disable certain piracy features, comment it out so that it doesn't
-                // trigger anti-piracy.
-//                .enableInstallerId(InstallerID.GOOGLE_PLAY)
-                //.enableInstallerId(InstallerID.AMAZON_APP_STORE)
-                // END OF THEMER TOUCHABLE OPTIONS
-                // TO RETAIN INTEGRITY, PLEASE DO NOT MODIFY ANY OTHER LINES
-
-                .callback(new PiracyCheckerCallback() {
-                    @Override
-                    public void allow() {
-                        beginSubstratumLaunch();
-                    }
-
-                    @Override
-                    public void dontAllow(PiracyCheckerError error) {
-                        String parse = String.format(getString(R.string.toast_unlicensed),
-                                getString(R.string.ThemeName));
-                        Toast toast = Toast.makeText(SubstratumLauncher.this, parse,
-                                Toast.LENGTH_SHORT);
-                        toast.show();
-                        finish();
-                    }
-                });
-
-        if (BASE_64_LICENSE_KEY.length() > 0) {
-            piracyChecker.enableGooglePlayLicensing(BASE_64_LICENSE_KEY);
-        }
-        if (APK_SIGNATURE_PRODUCTION.length() > 0) {
-            piracyChecker.enableSigningCertificate(APK_SIGNATURE_PRODUCTION);
-        }
-        piracyChecker.start();
-    }
-
-    /**
-     * Other variables/methods; do not modify
-     */
 
     private static final String SUBSTRATUM_PACKAGE_NAME = "projekt.substratum";
 
@@ -168,14 +91,6 @@ public class SubstratumLauncher extends Activity {
         finish();
     }
 
-    private void preparing() {
-        if (ENABLE_ANTI_PIRACY && !BuildConfig.DEBUG) {
-            startAntiPiracyCheck();
-        } else {
-            beginSubstratumLaunch();
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -193,7 +108,7 @@ public class SubstratumLauncher extends Activity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                preparing();
+                beginSubstratumLaunch();
                 finish();
             }
 
